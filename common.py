e75b7dac55827db5cfc98c0eeca8bdb7f0f0d20d
readysloth <readysloth@protonmail.com>
2022-07-19 15:20:50 +0300

    feat(common.py): Created functions to add variables to configs

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import os
+import re
 import logging
 
 class Colors:
@@ -19,3 +20,23 @@ if 'LOGLEVEL' in os.environ:
     LOGGER_LEVEL = getattr(logging,
                            os.environ['LOGLEVEL'],
                            LOGGER_LEVEL)
+
+
+def add_value_to_string_variable(filename, variable_name, value, quot='"', delim=' '):
+    variable_name_re = re.compile(fr'^\s*{variable_name}=')
+    variable_value_re = re.compile(fr'{quot}([^{quot}]*){quot}')
+    changed_lines = []
+    with open(filename, 'r') as file:
+        for l in file:
+            if variable_name_re.match(l):
+                changed_lines.append(variable_value_re.sub(fr'{quot}\1{delim}{value}{quot}', l))
+            else:
+                changed_lines.append(l)
+
+    with open(filename, 'w') as file:
+        file.writelines(changed_lines)
+
+
+def add_variable_to_file(filename, variable_name, value, quot='"')
+    with open(filename, 'a') as file:
+        file.write(f'{variable_name}={quot}{value}{quot}\n')
