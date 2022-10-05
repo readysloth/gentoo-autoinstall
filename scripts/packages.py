09db19cea139cb0311c6ae252919188e01bab7d6
readysloth <readysloth@protonmail.com>
2022-10-05 08:05:52 +0300

    exp(scripts/packages.py): Another attempt to solve circular dep of ncurses-gpm

--- scripts/packages.py
+++ scripts/packages.py
@@ -27,9 +27,8 @@ MASKS = [
 ]
 
 QUIRKED_PACKAGES = [
-    Package('sys-libs/ncurses', '--oneshot', use_flags='-gpm'), # should solve circular dep
-    Package('sys-libs/gpm'), # should solve circular dep
-    Package('sys-libs/ncurses', '--oneshot'), # should solve circular dep
+    # should solve circular dep later
+    Package('sys-libs/ncurses', env={'USE': '-gpm'}),
 ]
 
 
@@ -41,6 +40,7 @@ ESSENTIAL_PACKAGE_LIST = [
                              'notmpfs']),
     Package('app-shells/dash'),
     Package('@world', '-uDNv --with-bdeps=y --backtrack=100'),
+    Package('sys-libs/ncurses sys-libs/gpm'), # should solve circular dep
     Package('media-libs/libpng', use_flags='apng'),
     Package('app-editors/vim', use_flags='vim-pager perl terminal lua'),
     Package('sys-kernel/gentoo-sources', use_flags='symlink'),
