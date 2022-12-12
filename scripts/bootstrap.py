54429e1cab18eb4f7d238ef33a0d722f97c27146
readysloth <readysloth@protonmail.com>
2022-12-12 08:16:35 +0300

    feat(scripts/bootstrap.py): Added `qemu-wrapper` compilation to bootstrapping

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -13,6 +13,28 @@ import install_logger
 MOUNTPOINT = '/mnt/gentoo'
 
 
+def _compile_qemu_wrapper():
+    with open('qemu-wrapper.c', 'w') as f:
+        f.write("""
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envp) {
+    char *newargv[argc + 3];
+
+    newargv[0] = argv[0];
+    newargv[1] = "-cpu";
+    newargv[2] = "cortex-a53"; /* here you can set the cpu you are building for */
+
+    memcpy(&newargv[3], &argv[1], sizeof(*argv) * (argc -1));
+    newargv[argc + 2] = NULL;
+    return execve("/usr/bin/qemu-aarch64", newargv, envp);
+}
+""")
+    Executor.exec(Action('gcc -static qemu-wrapper.c -O2 -s -o qemu-wrapper',
+                         name='qemu wrapper compilation'))
+
+
 def _launch_ntpd():
     Executor.exec(Action('ntpd -q -g', name='time syncing'), do_crash=True)
 
@@ -61,6 +83,27 @@ def _mirrorselect():
     Executor.exec(Action(f'mirrorselect -s{common.MIRROR_COUNT} -4 -D', name='mirrorselect'), do_crash=True)
 
 
+def _binhost_setup():
+    arch = 'aarch64'
+    binfmt_register = ':'.join([f':{arch}',
+                                r'M',
+                                r':\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00',
+                                r'\xff\xff\xff\xff\xff\xff\xff\xfc\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff',
+                                r'/qemu-wrapper:'])
+    binary_setup = [Action(f'echo -1 > /proc/sys/fs/binfmt_misc/{arch}',
+                           name=f'binfmt {arch} unregister'),
+                    Action(f'echo "{binfmt_register}" > /proc/sys/fs/binfmt_misc/register',
+                           name='binfmt qemu-wrapper register')]
+    for a in binary_setup:
+        Executor.exec(a)
+
+    Executor.exec(Action('emerge --usepkgonly --oneshot --nodeps qemu',
+                         env={'ROOT': MOUNTPOINT},
+                         name='usermode qemu setup'))
+    _compile_qemu_wrapper()
+    shutil.copy('qemu-wrapper', MOUNTPOINT)
+
+
 def _final_bootstrap_configuration():
     final_actions = [Action(f'mkdir -p {MOUNTPOINT}/etc/portage/repos.conf', name='creating repos.conf folder'),
                      Action(f'cp /usr/share/portage/config/repos.conf {MOUNTPOINT}/etc/portage/repos.conf/gentoo.conf',
@@ -75,6 +118,7 @@ def _final_bootstrap_configuration():
                      Action(f'mount --make-rslave       {MOUNTPOINT}/dev', name='enslaving virtual fs')]
     for a in final_actions:
         Executor.exec(a, do_crash=True)
+    _binhost_setup()
 
 
 def _chroot_to_mnt():
