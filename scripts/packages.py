dbf7cb24782adad2727b1006e6c83fc92a309dc8
readysloth <readysloth@protonmail.com>
2023-01-07 19:29:25 +0300

    refact(scripts/packages.py): Removed unnecessary repetition

--- scripts/packages.py
+++ scripts/packages.py
@@ -366,28 +366,16 @@ ACTION_LIST = [
            name='grub liux cmdline'),
     Action('grub-mkconfig -o /boot/grub/grub.cfg',
            name='grub config creation'),
-    Action('rc-update add sysklogd default',
-           name='service update'),
-    Action('rc-update add cronie default',
-           name='service update'),
-    Action('rc-update add alsasound default',
-           name='service update'),
-    Action('rc-update add docker default',
-           name='service update'),
-    Action('rc-update add libvirtd default',
-           name='service update'),
-    Action('rc-update add dhpcd default',
-           name='service update'),
-    Action('rc-update add pulseaudio default',
-           name='service update'),
-    Action('rc-update add lvmetad boot',
-           name='service update'),
-    Action('rc-update add consolefont boot',
-           name='service update'),
-    Action('rc-update add dbus default',
-           name='service update'),
-    Action('rc-update add NetworkManager default',
-           name='service update'),
+] + [
+    Action(f'rc-update add {s} boot',
+           name=f'service {s} added to boot')
+    for s in ['lvmetad', 'consolefont']
+] + [
+    Action(f'rc-update add {s} default',
+           name=f'service {s} added to default')
+    for s in ['sysklogd', 'cronie', 'alsasound',
+              'docker', 'libvirtd', 'pulseaudio',
+              'dbus', 'NetworkManager']
 ]
 
 
